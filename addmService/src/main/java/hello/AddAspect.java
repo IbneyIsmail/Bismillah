603ccd80c8c663525c083d842e9ce3427e04af8d
Anees Ahmed <mdaneesahmed@gmail.com>
2018-03-09 16:13:40 +0530

    some changes related after and before

--- addmService/src/main/java/hello/AddAspect.java
+++ addmService/src/main/java/hello/AddAspect.java
@@ -1,6 +1,8 @@
 package hello;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -21,6 +23,17 @@ public class AddAspect {
 		//Advice
 		logger.info("  " + joinPoint.getKind());
 		logger.info("  " + joinPoint.getTarget());
-		logger.info(" Allowed execution for {}", joinPoint);
+		logger.info(" JoinPoint Details {}", joinPoint);
+	}
+	
+	@AfterReturning(value = "execution(* hello.*.*(..))", 
+			returning = "result")
+	public void afterReturning(JoinPoint joinPoint, Object result) {
+		logger.info("{} returned with value {}", joinPoint, result);
+	}
+	
+	@After(value = "execution(* hello.*.*(..))")
+	public void after(JoinPoint joinPoint) {
+		logger.info("after execution of {}", joinPoint);
 	}
 }
